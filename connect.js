848eda18390b7cef82bf712e5840e9505d041246
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-02-04 17:45:56 -0800

    installed dotenv, console.table, express, mysql2, and inquirer

---
+++ connect.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: process.env.PW,
+    database: "employee_db",
+  },
+  console.log(`Connected to the employee_db database.`)
+);
